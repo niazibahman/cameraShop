9f0a9b09edf1205751006ecc089d95277b397e50
b.niazi <niazi.2021.b@gmail.com>
2024-01-26 20:37:25 +0330

    add footer png's and component staticly

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+    <footer className="absolute bottom-6 inset-x-0 flex flex-row items-center justify-end lg:justify-between">
+        <img src="/svg/category-png.png" alt="" className="w-11/12 h-14 ml-2 hidden lg:flex"/>
+        <img src="/svg/basketCart-png.png" alt="" className="w-1/12 h-14"/>
+    </footer>
+    );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
+import Footer from "../components/footer";
+
 export default function Home(){
     return(
-        <div className="text-primery relative w-full lg:container mx-auto h-full">
-            <footer className="absolute bottom-8 inset-x-0 h-8 flex flex-row items-center justify-between">
-                <img src="/svg/category.svg" alt="" className="w-5/6  ml-2"/>
-                <img src="/svg/basketCart.svg" alt="" className="w-1/6 "/>
-            </footer>
-        </div>
+        <>
+            <img src="/svg/menu.svg" alt="" className="absolute top-4 right-4 w-8 h-8 lg:hidden"/>
+            <div className="text-primery relative w-full lg:container h-full">
+                <Footer/>
+            </div>
+        </>
     );
 }
